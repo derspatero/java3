f5e8ee7c9e78d83439d7717ab5936fd2c3f2e7af
Edward Lambke <elambke@yahoo.com>
2016-02-24 21:32:56 -0800

    input refactoring

--- A00892244Gis/src/a00892244/Gis.java
+++ A00892244Gis/src/a00892244/Gis.java
@@ -54,18 +54,16 @@ public class Gis {
 
 		try {
 
+			LocalDateTime startDate = LocalDateTime.now();
 			LOG.info("Starting program");
 			LOG.info("program arguments: " + Arrays.toString(args));
 
 			readData();
-			analyseData();
 			writeReport(args);
-
-			LocalDateTime startDate = LocalDateTime.now();
 
 			LocalDateTime endDate = LocalDateTime.now();
 			long timeDelta = startDate.until(endDate, ChronoUnit.MILLIS);
-			LOG.info("Duration: " + timeDelta + " ms\n");
+			LOG.info("Exiting.  Duration: " + timeDelta + " ms\n");
 
 		} catch (ApplicationException e) {
 			LOG.error(e.getMessage());
@@ -129,82 +127,65 @@ public class Gis {
 
 	private static void readData() throws ApplicationException {
 
+		Map<Integer, Persona> personas = new HashMap<Integer, Persona>();
+		games = new HashMap<String, Game>();
 		players = new HashMap<Integer, Player>();
+		
+		LOG.info("Reading from personas.dat");
+		PersonaReader personaReader = new PersonaReader("personas.dat");
+
+		while (personaReader.moreData()) {
+			Persona newPersona = personaReader.getNextPersona();
+			LOG.debug("New :  " + newPersona.toString());
+			personas.put(newPersona.getId(), newPersona);
+		}
+
+		LOG.debug(personas.toString());
+		
+		LOG.info("Reading from games.dat");
+		GameReader gameReader = new GameReader("games.dat");
+
+		while (gameReader.moreData()) {
+			Game newGame = gameReader.getNextGame();
+			LOG.debug("New " + newGame.toString());
+			games.put(newGame.getId(), newGame);
+		}
+		LOG.debug(games.toString());
+		
+		LOG.info("Reading from scores.dat");
+		ScoreReader scoreReader = new ScoreReader("scores.dat");
+
+		while (scoreReader.moreData()) {
+			Score score = scoreReader.getNextScore();
+			LOG.debug("New " + score.toString());
+			if (!personas.get(score.getPersonaId()).getGames().containsKey(score.getGameId())) {
+				personas.get(score.getPersonaId()).addGame(new Game(games.get(score.getGameId())));
+			}
+			Game game = personas.get(score.getPersonaId()).getGames().get(score.getGameId());
+			game.addScore(score);
+			LOG.debug("Added Game " + game.toString() + " to persona id " + personas.get(score.getPersonaId()).getId());
+			games.get(score.getGameId()).addScore(score);
+		}
 
 		LOG.info("Reading from players.dat");
 		PlayerReader playerReader = new PlayerReader("players.dat");
 
 		while (playerReader.moreData()) {
 			Player newPlayer = playerReader.getNextPlayer();
-			LOG.info("New Player: " + newPlayer.toString());
+			LOG.debug("New " + newPlayer.toString());
 			players.put(newPlayer.getIdentifier(), newPlayer);
 		}
-		LOG.info(players.toString());
-
-		games = new HashMap<String, Game>();
-
-		LOG.info("Reading from games.dat");
-		GameReader gameReader = new GameReader("games.dat");
-
-		while (gameReader.moreData()) {
-			Game newGame = gameReader.getNextGame();
-			LOG.info("New Game:  " + newGame.toString());
-			games.put(newGame.getId(), newGame);
-		}
-		LOG.info(games.toString());
-
-		scores = new ArrayList<Score>();
-
-		LOG.info("Reading from scores.dat");
-		ScoreReader scoreReader = new ScoreReader("scores.dat");
-
-		while (scoreReader.moreData()) {
-			Score newScore = scoreReader.getNextScore();
-			LOG.info("New Score:  " + newScore.toString());
-			scores.add(newScore);
-		}
-		LOG.info(scores.toString());
-
-		personas = new HashMap<Integer, Persona>();
-
-		LOG.info("Reading from personas.dat");
-		PersonaReader personaReader = new PersonaReader("personas.dat");
-
-		while (personaReader.moreData()) {
-			Persona newPersona = personaReader.getNextPersona();
-			LOG.info("New Persona:  " + newPersona.toString());
-			personas.put(newPersona.getId(), newPersona);
-		}
-
-		LOG.info(personas.toString());
-	}
-
-	private static void analyseData() {
-		Iterator<Score> iterator = scores.iterator();
-		while (iterator.hasNext()) {
-			Score score = iterator.next();
-			Game game;
-			Persona persona = personas.get(score.getPersonaId());
-			LOG.warn(score.toString() + " goes to " + persona.toString());
-			if (!persona.getGames().containsKey(score.getGameId())) {
-				persona.addGame(new Game(games.get(score.getGameId())));
-			}
-			game = persona.getGames().get(score.getGameId());
-			game.addScore(score);
-			LOG.info("Added Game " + game.toString() + " to persona id " + persona.getId());
-
-			games.get(score.getGameId()).addScore(score);
-			LOG.info("Added Score " + score.toString() + " to Game " + score.getGameId());
-		}
-
+		LOG.debug(players.toString());
+		
+		LOG.info("Assigning personas to players");
 		for (int key : personas.keySet()) {
 			Player player = players.get(personas.get(key).getPlayerId());
 			player.addPersona(personas.get(key));
 			players.put(player.getIdentifier(), player);
-			LOG.info("Added Persona " + personas.get(key) + " to player id " + player.getIdentifier());
+			LOG.debug("Added Persona " + personas.get(key) + " to player id " + player.getIdentifier());
 		}
-
-		LOG.info(players.toString());
+		
+		LOG.debug(players.toString());
 	}
 
 }
